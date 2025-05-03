afd56754939abb5026d75bef0ffe9781bcca646d
Anwar Rahim <anwargithub@gmail.com>
2025-05-03 14:30:46 +0500

    Refactor project data structure and enhance ProjectsPage component

--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -5,43 +5,55 @@ import emailImgae from '../assets/email/images/image.jpg'
     const projectData = [
         {
             projectName : 'Email Subscription',
-            mainImg: {emailImgae},
+            mainImg: emailImgae,
             description: "Design for email subscription",
-            buttonLink: '/email-subs'
+            buttonLink: '/email-subs',
+            category:'web'
         },
         {
             projectName: 'Pricing Grid',
-            mainImg:{emailImgae},
+            mainImg:emailImgae,
             description:"Design for pricing Grid",
-            buttonLink: '/pricing-grid'
+            buttonLink: '/pricing-grid',
+            category:'web'
+
         },
         {
             projectName:'Product Modal',
-            mainImg: {emailImgae},
+            mainImg: emailImgae,
             description:'Design for product Grid',
-            buttonLink:'/product-modal'
+            buttonLink:'/product-modal',
+            category:'web'
         },
         {
             projectName:'image-gallery',
-            mainImg:{emailImgae},
+            mainImg:emailImgae,
             description:'Desing for image gallery',
-            buttonLink: '/image-gallery'
+            buttonLink: '/image-gallery',
+            category:'web'
         }
 
     ]
    
   const ProjectsPage = ()=>{
 
+    
 
     return(
         <>
-             {<EmailSubs 
+             {/* {<EmailSubs 
         mainImage = {emailImgae}
         name = "Anwar" 
         mainHeading = "Get diet and fitness tips in your inbox "
         mainPara = "Eat better and excercise better. Sign up for the diet and Fitness"
         
-        /> }
+        /> } */}
+        <div>
+            
+        </div>
+      
+
+
         </>
     )
   }
